90cc33d3cc6fc2f8c52772b4244975554aece40c
Aiden Chulhwan Ahn <aiden.c.ahn@gmail.com>
2020-02-05 13:49:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table # -*- coding: utf-8 -*-
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
